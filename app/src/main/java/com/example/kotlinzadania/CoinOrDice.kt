f80a14e28d4ba2fe2c9366237705e5efd6f6da10
jurekg1 <jurek@poczta.pl>
2024-12-09 11:26:46 +0100

    tested the program

--- app/src/main/java/com/example/kotlinzadania/CoinOrDice.kt
+++ app/src/main/java/com/example/kotlinzadania/CoinOrDice.kt
@@ -26,6 +26,16 @@ class Dice: RollingTool
     override fun roll()
     {
         var roll = Random.nextInt(1,6)
+        println(roll)
     }
+}
+
+fun main()
+{
+    var dice = Dice()
+    dice.roll()
+
+    var coin = Coin()
+    coin.roll()
 }
 
