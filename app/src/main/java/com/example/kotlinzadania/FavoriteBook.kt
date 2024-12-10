e7c94e8ba55024e874dcc6aa3988b3fd491ff920
jurekg1 <jurek@poczta.pl>
2024-12-10 15:55:30 +0100

    added some books and tested the showBooks method

--- app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
+++ app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
@@ -6,7 +6,28 @@ data class Book(val title: String, val author: String, val year: Int)
 
 object BookManager
 {
-    val books = mutableListOf<Book>()
+    val books = mutableListOf(
+        Book("The Great Gatsby", "F. Scott Fitzgerald", 1925),
+        Book("Tender is the Night", "F. Scott Fitzgerald", 1934),
+        Book("1984", "George Orwell", 1949),
+        Book("Animal Farm", "George Orwell", 1945),
+        Book("To Kill a Mockingbird", "Harper Lee", 1960),
+        Book("Go Set a Watchman", "Harper Lee", 1960),
+        Book("Pride and Prejudice", "Jane Austen", 1813),
+        Book("Sense and Sensibility", "Jane Austen", 1811),
+        Book("Moby-Dick", "Herman Melville", 1851),
+        Book("The Confidence-Man", "Herman Melville", 1857),
+        Book("War and Peace", "Leo Tolstoy", 1869),
+        Book("Anna Karenina", "Leo Tolstoy", 1877),
+        Book("The Catcher in the Rye", "J.D. Salinger", 1951),
+        Book("Franny and Zooey", "J.D. Salinger", 1961),
+        Book("The Hobbit", "J.R.R. Tolkien", 1937),
+        Book("The Lord of the Rings", "J.R.R. Tolkien", 1954),
+        Book("Crime and Punishment", "Fyodor Dostoevsky", 1866),
+        Book("The Brothers Karamazov", "Fyodor Dostoevsky", 1880),
+        Book("The Adventures of Sherlock Holmes", "Arthur Conan Doyle", 1892),
+        Book("The Memoirs of Sherlock Holmes", "Arthur Conan Doyle", 1892)
+    )
 
     fun addBook()
     {
@@ -40,7 +61,9 @@ object BookManager
     {
         books.forEach{println(it)}
     }
+}
 
-
-
+fun main()
+{
+    BookManager.showBooks()
 }
