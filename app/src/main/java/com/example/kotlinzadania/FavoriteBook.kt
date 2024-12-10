56b85d74fe6362de581e0353aee56b3ad67f9136
jurekg1 <jurek@poczta.pl>
2024-12-10 15:45:17 +0100

    improved the filterBy function by displaying the results

--- app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
+++ app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
@@ -15,6 +15,27 @@ object BookManager
         println("Podaj rok wydania: ")
         val year=readln().toInt()
         books.add(Book(title,author,year))
+        println("Udało się dodać książkę. ")
+    }
+
+    fun filterBy()
+    {
+        println("Po czym chcesz sortować swoje książki: a - autor | y - rok wydania")
+        var choice = readln()
+        if (choice=="a")
+        {
+            println("Podaj autora: ")
+            var a = readln()
+            var filtered = books.filter{it.author.equals(a)}
+            filtered.forEach{println(it)}
+        }
+        else if (choice=="y")
+        {
+            println("Podaj rok: ")
+            var y = readln().toInt()
+            var filtered = books.filter{it.year.equals(y)}
+            filtered.forEach{println(it)}
+        }
     }
 
 
