668b1680eaabefb4029fea8b76db4d850b961ba5
jurekg1 <jurek@poczta.pl>
2024-12-10 15:33:56 +0100

    created addBook() method

--- app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
+++ app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
@@ -6,5 +6,16 @@ object BookManager
 {
     val books = mutableListOf<Book>()
 
+    fun addBook()
+    {
+        println("Podaj tytuł: ")
+        val title=readln()
+        println("Podaj autora: ")
+        val author=readln()
+        println("Podaj rok wydania: ")
+        val year=readln().toInt()
+        books.add(Book(title,author,year))
+    }
+
 
 }
