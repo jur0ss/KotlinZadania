ac079277ec0ecd66069484fbf3d0954a67dea644
jurekg1 <jurek@poczta.pl>
2024-12-10 15:13:01 +0100

    added more quotes and tested the program

--- app/src/main/java/com/example/kotlinzadania/RandomQuote.kt
+++ app/src/main/java/com/example/kotlinzadania/RandomQuote.kt
@@ -4,7 +4,18 @@ import kotlin.random.Random
 
 object Quotes
 {
-    val quotes = mutableListOf<String>("elo","siema","cześć")
+    val quotes = mutableListOf(
+        "The best way to predict the future is to invent it.",
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        "Do what you can, with what you have, where you are.",
+        "In the middle of every difficulty lies opportunity.",
+        "Don't watch the clock; do what it does. Keep going.",
+        "The only way to do great work is to love what you do.",
+        "Life is 10% what happens to us and 90% how we react to it.",
+        "An unexamined life is not worth living.",
+        "Happiness is not something ready-made. It comes from your own actions.",
+    )
+
 
     fun addQuote()
     {
@@ -22,5 +33,6 @@ object Quotes
 
 fun main()
 {
+    Quotes.addQuote()
     Quotes.getRandomQuote()
 }
