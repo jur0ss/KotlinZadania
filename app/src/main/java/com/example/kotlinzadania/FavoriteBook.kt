ccd8ee0adf443615aa8838f8f01bd2e69ef6aabe
jurekg1 <jurek@poczta.pl>
2024-12-10 15:50:54 +0100

    created two different functions to filter, added showbooks() method

--- app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
+++ app/src/main/java/com/example/kotlinzadania/FavoriteBook.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinzadania
 
+import com.example.kotlinzadania.BookManager.books
+
 data class Book(val title: String, val author: String, val year: Int)
 
 object BookManager
@@ -18,25 +20,27 @@ object BookManager
         println("Udało się dodać książkę. ")
     }
 
-    fun filterBy()
-    {
-        println("Po czym chcesz sortować swoje książki: a - autor | y - rok wydania")
-        var choice = readln()
-        if (choice=="a")
-        {
-            println("Podaj autora: ")
-            var a = readln()
-            var filtered = books.filter{it.author.equals(a)}
-            filtered.forEach{println(it)}
-        }
-        else if (choice=="y")
-        {
-            println("Podaj rok: ")
-            var y = readln().toInt()
-            var filtered = books.filter{it.year.equals(y)}
-            filtered.forEach{println(it)}
-        }
+    fun filterByAuthor() {
+        println("Podaj autora: ")
+        var a = readln()
+        var filtered = books.filter { it.author == a }
+        filtered.forEach { println(it) }
+
     }
+
+    fun filterByYear()
+    {
+        println("Podaj rok: ")
+        var y = readln().toInt()
+        var filtered = books.filter{ it.year == y }
+        filtered.forEach{println(it)}
+    }
+
+    fun showBooks()
+    {
+        books.forEach{println(it)}
+    }
+
 
 
 }
