b4451718345b1cc0ff21c3e645ef2a67e4eb350d
jurekg1 <jurek@poczta.pl>
2024-12-16 11:13:47 +0100

    Added an interactive menu which allows the user to show his steps or reset them

--- app/src/main/java/com/example/kotlinzadania/StepCounter.kt
+++ app/src/main/java/com/example/kotlinzadania/StepCounter.kt
@@ -28,10 +28,36 @@ fun main()
 {
     var Krokomierz = StepCounter()
     var i = 0
-    while (i<=5000)
+    while (i<=4999)
     {
-        Krokomierz.addStep()
         i++
+        Krokomierz.addStep()
     }
-
+    while (true)
+    {
+        println("Co chcesz wykonać: 1 - pokaż kroki | 2 - zresetuj kroki | 3 - zakończ")
+        var choice = readln().toInt()
+        if(choice==1)
+        {
+            Krokomierz.showSteps()
+        }
+        else if (choice==2)
+        {
+            println("Czy na pewno chcesz zresetować swoje kroki?: ")
+            var sure = readln()
+            if (sure=="tak")
+            {
+                println("Zresetowano kroki")
+                Krokomierz.resetSteps()
+            }
+            else
+            {
+                println("Nie zresetowano kroków")
+            }
+        }
+        else if (choice==3)
+        {
+            break
+        }
+    }
 }
