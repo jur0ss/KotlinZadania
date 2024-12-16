928aa1923d7c1ba44e1e691c19719c6ac3101644
jurekg1 <jurek@poczta.pl>
2024-12-16 10:16:05 +0100

    Created different weather classes implementing Weather interface

--- app/src/main/java/com/example/kotlinzadania/WeatherApp.kt
+++ app/src/main/java/com/example/kotlinzadania/WeatherApp.kt
@@ -1,3 +1,41 @@
 package com.example.kotlinzadania
 
 data class City (val name: String)
+
+interface Weather
+{
+    fun showWeather()
+}
+
+class Sunny: Weather
+{
+    override fun showWeather() {
+        println("Jutro będzie słonecznie")
+    }
+}
+
+class Rainy: Weather
+{
+    override fun showWeather() {
+        println("Jutro będzie deszczowo")
+    }
+}
+
+class Snowy: Weather
+{
+    override fun showWeather() {
+        println("Jutro będzie padał śnieg")
+    }
+}
+
+class Cloudy: Weather
+{
+    override fun showWeather() {
+        println("Jutro będzie zachmurzenie")
+    }
+}
+
+fun main()
+{
+
+}
