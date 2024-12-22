5b61f40eff22799b1057d7e68eedac0c72165860
jurekg <jurek.gryz2007@gmail.com>
2024-12-22 15:42:34 +0100

    Added some results

--- app/src/main/java/com/example/kotlinzadania/SportsResults.kt
+++ app/src/main/java/com/example/kotlinzadania/SportsResults.kt
@@ -5,6 +5,15 @@ data class Results(val points: Int)
 object ResultsAnalyzer
 {
     val results = mutableListOf(
-        Results(15)
+        Results(15),
+        Results(12),
+        Results(19),
+        Results(25),
+        Results(30),
+        Results(8),
+        Results(22),
+        Results(17),
+        Results(27),
+        Results(14)
     )
 }
